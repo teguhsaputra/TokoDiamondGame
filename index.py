20562d761c298041af4e9eab18b254c712a163b1
Teguh Saputra <teguhsaputra01@gmail.com>
2022-11-24 17:16:14 +0700

    Yeee Kelar

--- index.py
+++ index.py
@@ -3,82 +3,20 @@ import time
 import pandas as pd
 import numpy as np
 from datetime import datetime
+from tabulate import tabulate
 
 def clear_screen():
     _ = os.system('cls')
 
-
-# def Login():
-    
-#     print('''
-#              SELAMAT DATANG DI 
-#         DIAMOND GAME STORE MURAH NAMPOL
-#               SILAHKAN LOGIN
-# ==============================================
-# ''')
-#     username = input("Masukkan Username : ")
-#     password = input("Masukkan Password : ")
-#     akses = 2
-
-#     if(username == "member" and password == "member" and akses == 1):
-#         Member()
-#     elif(username == "admin" and password == "admin" and akses == 2):
-#         Admin()
-#     else:
-#         print()
-#         print("Password Salah, Silahkan Login kembali !")
-#         time.sleep(3)
-#         clear_screen()
-#         Login()
-
-# def Member():
-#     print('''
-# Pilih Game 
-# 1. Mobile Legends
-# 2. Free Fire
-# 3. PUBG
-# ==================
-# ''')
-#     keranjang = []
-#     pilihgame = int(input("Masukkan Jenis Game : "))
-#     keranjang.insert = [pilihgame]
-
-#     idgame = input("Masukkan ID Game : ")
-#     keranjang.insert = [idgame]
-
-#     pilihdiamond = int(input("Masukkan Jumlah Diamond : "))
-#     keranjang.insert = [pilihdiamond]
-#     clear_screen()
-
-
-
-# def Admin():
-#     dataHarga = {
-#         "Diamond" : [14,42,86,172,257,347,429],
-#         "Harga" : [3999,10999,19999,39999,59999,80000,100000]
-#     }
-#     showData = pd.DataFrame(dataHarga)
-#     print(showData)
-#     print()
-#     pilihdiamond = int(input("Pilih Diamond : "))
-#     hargaDiamond = dataHarga["Harga"][pilihdiamond]
-#     print("Harga Diamond : ",hargaDiamond)
-#     jumlahBayar = int(input("Masukkan Jumlah Bayar : "))
-#     total = jumlahBayar - hargaDiamond
-#     print(50*"=")
-#     print("Kembalian = ", total)
-#     print("Terimakasih Telah Membeli Diamond Kami")
-#Login()
-# Admin()
-
 df_login = pd.read_json('db_login.json')
 df_game = pd.read_json('db_game.json')
 df_diamond = pd.read_json('db_diamond.json')
+df_penjualan = pd.read_csv("db_penjualan.csv")
 cart = []
 now = datetime.now()
 dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-# print(df_login["Username"])
-# temp_user = []
+list_penjualan = []
+
 def check_login(username,password):
     for dts in range(0, df_login["Username"].size):
         if df_login["Username"].values[dts] == username and df_login["Password"].values[dts] == password:
@@ -118,6 +56,39 @@ def Print():
     print("Terimakasih Telah Membeli Diamond")
     print("Di GAME STORE MURAH NAMPOL")
 
+def Lanjut_Def():
+    lanjut = input("Apakan Anda ingin membeli lagi ? <y/n> ")
+    if lanjut == "y" or lanjut == "Y":
+        cart.clear()
+        clear_screen()
+        member()            
+    elif lanjut == "n" or lanjut == "N":
+        clear_screen()
+        login()
+    else:
+        Lanjut_Def()
+
+def InputPenjualan():
+    global df_penjualan
+
+    list_penjualan = dict()
+
+    list_penjualan["NameGame"] = [cart[0]]
+    list_penjualan["IdGame"] = [cart[1]]
+    list_penjualan["Diamond"] = [cart[2]]
+    list_penjualan["Harga"] = [cart[3]]
+    list_penjualan["TanggalBeli"] = [cart[4]]
+
+    tmp = pd.DataFrame.from_dict(list_penjualan)
+
+    df_penjualan = pd.concat([df_penjualan, tmp], ignore_index=True)
+    df_penjualan.to_csv("db_penjualan.csv", index=False)
+
+    # print(list_penjualan)
+    # df_kirimdata = pd.DataFrame(list_penjualan)
+    # df_kirimdata.to_csv('db_penjualan.csv')
+    
+
 def Total_Def():
     print("Nama Game : ",cart[0])
     print("ID Game : ",cart[1])
@@ -139,20 +110,14 @@ def Total_Def():
         print("Terimakasih Telah Membeli Diamond")
         print("Di GAME STORE MURAH NAMPOL")
         print(35*"=")
-        lanjut = input("Apakan Anda ingin membeli lagi ? <y/n> ")
-        if lanjut == "y":
-            cart.clear()
-            clear_screen()
-            member()            
-        else:
-            clear_screen()
-            login()
-        
-
+        InputPenjualan()
+        Lanjut_Def()
 
 def Game_Def():
-    print(df_game.to_string(index=False))
-    print(50*"=")
+    data = pd.DataFrame(df_game)
+    print(tabulate(data, headers='keys', tablefmt='psql',showindex=False))
+    # print(df_game.to_string(index=False))
+    # print(50*"=")
     KodeGame = int(input("Masukkan KodeGame : "))
     ReturnKodeGame,Game = check_kodegame(KodeGame)
 
@@ -169,8 +134,10 @@ def Game_Def():
         Game_Def()
 
 def Diamond_Def():
-    print(df_diamond.to_string(index=False))
-    print(50*"=")
+    data = pd.DataFrame(df_diamond)
+    print(tabulate(data, headers='keys', tablefmt='psql', showindex=False))
+    # print(df_diamond.to_string(index=False))
+    # print(50*"=")
     KodeDiamond = int(input("Masukkan KodeDiamond : "))
     ReturnKodeDiamond,Diamond,Harga = check_kodediamond(KodeDiamond)
     if ReturnKodeDiamond == True:
@@ -190,10 +157,47 @@ def member():
     Diamond_Def()
     Total_Def()
 
-def admin():
-    print("Ini halaman admin")
+def print_penjualan():
+    clear_screen()
+    data = pd.DataFrame(df_penjualan)
+    print(tabulate(data, headers='keys', tablefmt='psql'))
+    print()
+    print("{:>80}".format("Total Pendapatan : " + str(data['Harga'].sum())))
+    print('''
+Menu : 
+[1] Cetak Laporan Keuangan
+[2] Kembali Ke Menu
+''')
+    lanjut = int(input("Pilih menu <1/2> ?  "))
+    if lanjut == 1:
+        os.startfile("db_penjualan.csv", "print")
+        print_penjualan()            
+    elif lanjut == 2:
+        clear_screen()
+        admin()
+    else:
+        clear_screen()
+        print_penjualan()
 
+def admin():
+    clear_screen()
+    print('''
+            SELAMAT DATANG 
+                ADMIN
+          SILAHKAN PILIH MENU
+========================================
+[1] Print Laporan Penjualan
+[2] Logout
+''')
+    menu = int(input("Pilih Menu : "))
+    if menu == 1:
+        print_penjualan()
+    elif menu  == 2:
+        login()
+    else:
+        admin()
 def login():
+    clear_screen()
     print('''
              SELAMAT DATANG DI 
         DIAMOND GAME STORE MURAH NAMPOL
@@ -225,115 +229,7 @@ def login():
 clear_screen()
 login()
 
+# print_penjualan()
 
-# for dts in range(0, df_login["Username"].size):
-#     if df_login["Username"].values[dts] != username or df_login["Password"].values[dts] == password:
-#         print("Username / Password Salah")
-#         break
-#     elif df_login["Username"].values[dts] == username and df_login["Password"].values[dts] == password:
-#         print("ada datanya")
-#         if df_login["HakAkses"].values[dts] == 0:
-#             print("Ini Halaman Member")
-#             break
-#             # Admin()
-#         else:
-#             # Member()
-#             print("ini Halaman Admin")
-#             break
-#     else:
-#         error = "Username/Password salah!"
-# print(error)
-    # print(df_login.values[dts])
-
-#     def MobileLegends():
-#         id_ml = int(input("Masukkan ID : "))
-#         server_ml = int(input("Masukkan Server : "))
-#         nominal_diamond =     
-
-# print('''
-#             SELAMAT DATANG DI 
-#         DIAMOND GAME STORE MURAH NAMPOL
-# ''')
-# nama = input("MASUKAN NAMA : ")
-# id_game=input("MASUKAN ID GAME :")
-# saldo=int(input("MASUKAN UANG  :"))
-# print('''
-# ------------------------------------------
-# NO |          GAME                       |
-# ------------------------------------------
-# 1  | MOBILE LEGEND                       |
-# 2  | CLASH OF CLANS                      |
-# 3  | FREE FIRE                           |
-# ------------------------------------------''')
-# def pembeliangame():
-#   print(f'''
-#   ------------------------------------------------------
-#                         {namagame}                  |
-#   ------------------------------------------------------
-#   1 | 50  DIAMOND   `                    |  Rp.10.000  |
-#   2 | 100 DIAMOND                        |  Rp.20.000  |
-#   3 | 500 DIAMOND                        |  Rp.30.000  |
-#   ------------------------------------------------------
-#   ''')
-  
-#   diamond = int(input("[1/2/3] Silahkan pilih jumlah diamond: "))
-#   if diamond == 1:
-#     dm = 100
-#     harga = 10000
-#     sisa_saldo = saldo - harga
-#     print('''
-#     ---------------------------------------
-#                 DATA PEMBELIAN
-#     ---------------------------------------
-#     ''')
-#     print("     Nama pembeli       =",nama)
-#     print("     Game               =",namagame)
-#     print("     Jumlah diamond     =",dm)
-#     print("     Harga total        = Rp.",harga)
-#     print("     Saldo tersisa      = Rp.",sisa_saldo)
-#   elif diamond == 2:
-#     dm = 300
-#     harga = 20000
-#     sisa_saldo = saldo - harga
-#     print('''
-#     ---------------------------------------
-#                 DATA PEMBELIAN
-#     ---------------------------------------
-#     ''')
-#     print("     Nama pembeli       =",nama)
-#     print("     Game               =",namagame)
-#     print("     Jumlah diamond     =",dm)
-#     print("     Harga total        = Rp.",harga)
-#     print("     Saldo tersisa      = Rp.",sisa_saldo)
-#   elif diamond == 3:
-#     dm = 500
-#     harga = 30000
-#     sisa_saldo = saldo - harga
-#     print('''
-#     ---------------------------------------
-#                 DATA PEMBELIAN
-#     ---------------------------------------
-#     ''')
-#     print("     Nama pembeli       =",nama)
-#     print("     Game               =",namagame)
-#     print("     Jumlah diamond     =",dm)
-#     print("     Harga total        = Rp.",harga)
-#     print("     Saldo tersisa      = Rp.",sisa_saldo)
-#   else:
-#     print("\nError!!!")
-#     print("\nJumlah diamond tidak ditemukan!")
-
-# game = int(input("[1/2/3] SILAHKAN PILIH GAME : "))
-
-# if game == 1:
-#   namagame = "MOBILE LEGEND"
-#   pembeliangame()
-# elif game == 2:
-#   namagame = "CLASH OF CLANS"
-#   pembeliangame()
-# elif game == 3:
-#   namagame = "FREE FIRE"
-#   pembeliangame()
-# else:
-#   print("\nError!!!")
-#   print("Game tidak ditemukan!")
+# print_penjualan()
+# InputPenjualan()
